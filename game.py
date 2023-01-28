1383e9e98b5266501980623110235c94c44d1ee3
yangzhihui <744017072@qq.com>
2023-01-28 18:41:06 +0800

    添加游戏功能

---
+++ game.py
@@ -0,0 +1,2 @@
+def games():
+    print("游戏")
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from video import video
 from article import art
 from picture import pic
 from cartoon import cart
+from game import games
 def main():
     print_main()
     video()
     art()
     pic()
     cart()
+    games()
 
 if __name__ == "__main__":
     main()
